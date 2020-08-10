3548dd605e689c5d95b223e72e5d7d7516084520
LynnAmsbury <Lynn8542@gmail.com>
2020-08-10 10:14:38 -0600

    Added update action in Users controller, added update route, verified functionality with Postman.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,11 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def update
+    @user.update(user_params)
+    render json: @user
+  endg
+
   private
 
   def user_params
